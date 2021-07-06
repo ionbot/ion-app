a91500b48389edcd1f145f63bb52745bc1f42849
xencodes <xencodes@icloud.com>
2021-07-06 20:10:46 +0530

    added gulp file

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+const gulp = require("gulp");
+const run = require("gulp-run");
+const fs = require("fs");
+
+const defaultSession = {
+  apiId: "",
+  apiHash: "",
+  session: "",
+};
+
+gulp.task("build-server", (cb) => {
+  run("npm run build:server").exec("", () => {
+    cb();
+  });
+});
+
+gulp.task("build-client", (cb) => {
+  run("npm run build:client").exec("", () => {
+    cb();
+  });
+});
+
+gulp.task("release", (cb) => {
+  gulp.task("release");
+  gulp.src("packages/bot/build/**").pipe(gulp.dest("dist/"));
+  gulp.src("packages/bot/package.json").pipe(gulp.dest("dist/"));
+  gulp.src("packages/dashboard/build/**").pipe(gulp.dest("dist/dashboard"));
+
+  fs.writeFileSync("dist/session.json", JSON.stringify(defaultSession));
+
+  cb();
+});
