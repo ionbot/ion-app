d1b1b80f52e1eb5c50eb41573212756d5fe834ae
xencodes <xencodes@icloud.com>
2021-07-08 22:13:44 +0530

    add default url

--- packages/dashboard/src/index.js
+++ packages/dashboard/src/index.js
@@ -8,11 +8,15 @@ import { ChakraProvider } from "@chakra-ui/react";
 
 import defaultTheme from "./themes/default";
 
-const { REACT_APP_HOST } = process.env;
+let { REACT_APP_HOST: host } = process.env;
+
+if (!host) {
+  host = "http://localhost:4200"; // default use localhost
+}
 
 ReactDOM.render(
   <ChakraProvider theme={defaultTheme}>
-    <Provider url={REACT_APP_HOST + "/api"}>
+    <Provider url={host + "/api"}>
       <Router />
     </Provider>
   </ChakraProvider>,
