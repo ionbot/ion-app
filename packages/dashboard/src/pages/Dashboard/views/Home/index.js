bef6ed722ad76f5bda542bb70e50ccfd3c2ed779
xencodes <xencodes@icloud.com>
2021-07-09 13:44:59 +0530

    moved userbot toggle to homepage

--- packages/dashboard/src/pages/Dashboard/views/Home/index.js
+++ packages/dashboard/src/pages/Dashboard/views/Home/index.js
@@ -1,15 +1,25 @@
-import { Box, GridItem, Heading, Text, SimpleGrid } from "@chakra-ui/react";
+import {
+  Box,
+  GridItem,
+  Heading,
+  Text,
+  SimpleGrid,
+  Button,
+} from "@chakra-ui/react";
 import { UserBotStore } from "../../../../store/userbot.store";
+import socket from "../../providers/socket.io";
 
 export default () => {
   const userbot = UserBotStore.useState((s) => s);
 
-  const StatusCard = ({ title, subTitle }) => (
+  //todo: separate this component
+  const StatusCard = ({ title, subTitle, action }) => (
     <Box p={4} borderWidth="1px" rounded="lg" bg="white">
       <Heading size="lg">{subTitle}</Heading>
       <Text fontSize="sm" color="gray.500" mt={2}>
         {title}
       </Text>
+      <Box mt={3}>{action}</Box>
     </Box>
   );
   return (
@@ -19,6 +29,17 @@ export default () => {
           <StatusCard
             title={"Userbot Status"}
             subTitle={userbot.status == 1 ? "Running" : "Stopped"}
+            action={
+              <Button
+                colorScheme={userbot.status == 0 ? "brand" : "red"}
+                size="sm"
+                onClick={() => {
+                  socket.emit(`${userbot.status == 0 ? "start" : "stop"}-bot`);
+                }}
+              >
+                {userbot.status == 0 ? "Start" : "Stop"} Userbot
+              </Button>
+            }
           />
         </GridItem>
 
