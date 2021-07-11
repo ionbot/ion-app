34a42950d19de9f340061f9d83c24cad5e63a9cc
xencodes <xencodes@icloud.com>
2021-07-11 13:54:33 +0530

    no need for session file

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,6 @@ const zip = require("gulp-zip");
 const fs = require("fs");
 const { version } = require("./lerna.json");
 
-const defaultSession = {
-  apiId: 0,
-  apiHash: "",
-  session: "",
-};
-
 gulp.task("build-server", (cb) => {
   run("npm run build:server").exec("", () => {
     cb();
@@ -29,7 +23,6 @@ gulp.task("release", (cb) => {
   gulp.src("app.json").pipe(gulp.dest("dist/"));
   gulp.src("packages/dashboard/build/**").pipe(gulp.dest("dist/dashboard"));
 
-  fs.writeFileSync("dist/session.json", JSON.stringify(defaultSession));
   fs.writeFileSync("dist/.env", `NODE_ENV=production\nMONGO=`);
 
   gulp
