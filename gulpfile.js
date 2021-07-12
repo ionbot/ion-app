fd1ad194fe8cdf766f4ff913dd2e1889972dc72c
xencodes <xencodes@icloud.com>
2021-07-12 13:17:35 +0530

    fix: update package json version

--- gulpfile.js
+++ gulpfile.js
@@ -37,6 +37,11 @@ gulp.task("dist", (cb) => {
     )
   );
 
+  const lerna = require("./lerna.json");
+  const _package = require("./package.json");
+  _package["version"] = lerna["version"];
+  fs.writeFileSync("package.json", JSON.stringify(_package, null, 2));
+
   cb();
 });
 
