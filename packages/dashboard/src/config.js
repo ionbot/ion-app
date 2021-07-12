dd2ae6a65e13dc1b6d8750848449364d1b659ec2
xencodes <xencodes@icloud.com>
2021-07-12 10:05:20 +0530

    fix

--- packages/dashboard/src/config.js
+++ packages/dashboard/src/config.js
@@ -1,5 +1,7 @@
+let { REACT_APP_HOST } = process.env;
+
 export const HOST = REACT_APP_HOST
   ? REACT_APP_HOST
   : window.location.host.includes("localhost")
   ? "http://localhost:4200"
-  : ""; // sets the current website as host
+  : ""; // set the current website as host
