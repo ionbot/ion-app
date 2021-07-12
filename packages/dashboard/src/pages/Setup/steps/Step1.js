54b9fc1cf8f4bbd6c82efc0f7c047fbefe23c5a1
Sρι∂у <rshouriesh@gmail.com>
2021-07-12 08:46:27 +0530

    More Changes Improved a Lot

--- packages/dashboard/src/pages/Setup/steps/Step1.js
+++ packages/dashboard/src/pages/Setup/steps/Step1.js
@@ -1,4 +1,4 @@
-import { Stack, Input, FormControl, FormLabel } from "@chakra-ui/react";
+import { Stack, Input, FormControl, FormLabel, FormHelperText } from "@chakra-ui/react";
 import { SetupStore } from "../../../store/setup.store";
 
 export default () => {
@@ -11,26 +11,21 @@ export default () => {
   return (
     <Stack>
       <FormControl>
-        <FormLabel>API ID (Get This Value From  "https://my.telegram.org")</FormLabel>
+        <FormLabel>API ID</FormLabel>
         <Input placeholder="3499832" id="apiId" onChange={handelChange} />
+        <FormHelperText>Please Get this Value From "https://my.telegram.org"</FormHelperText> 
       </FormControl>
 
       <FormControl>
         <FormLabel>API HASH (Get This Value From  "https://my.telegram.org")</FormLabel>
-        <Input
-          placeholder="a2sd889qw88dq9du9dq8w7ud9o"
-          id="apiHash"
-          onChange={handelChange}
-        />
+        <Input placeholder="a2sd889qw88dq9du9dq8w7ud9o" id="apiHash" onChange={handelChange} />
+        <FormHelperText>Please Get this Value From "https://my.telegram.org"</FormHelperText>
       </FormControl>
 
       <FormControl>
         <FormLabel>Phone Number</FormLabel>
-        <Input
-          placeholder="+91XXXXXXXXXX"
-          id="phoneNumber"
-          onChange={handelChange}
-        />
+        <Input placeholder="+91XXXXXXXXXX" id="phoneNumber" onChange={handelChange} />
+        <FormHelperText>Enter Your Mobile Number With Country Code Here</FormHelperText>
       </FormControl>
     </Stack>
   );
