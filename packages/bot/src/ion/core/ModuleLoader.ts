2cc2415e826d6e3902fac8b2c54f36891288513a
Roj Serbest <rojserbest@icloud.com>
2021-07-13 10:23:19 +0300

    Allow multiple handlers in a module
    Update ModuleLoader taking advantage of scope and adding default value to mode and update all modules

--- packages/bot/src/ion/core/ModuleLoader.ts
+++ packages/bot/src/ion/core/ModuleLoader.ts
@@ -1,31 +1,52 @@
+import { TelegramClient } from "telegram";
 import { NewMessage, NewMessageEvent } from "telegram/events";
 // import * as appConfig from "../providers/app-config";
 import * as moduleConfig from "../providers/module-config";
-import { allModules } from "../modules";
+import modules from "../modules";
 import Client from "./Client";
+import IonHandler from "./IonHandler";
 
 const defaultPrefixes = ["."];
 
 const escapeForRegExp = (s: string) => s.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
 
+export interface ModuleMeta {
+  slug: string; // may be used for identification of that module
+  name: string;
+  description: string;
+}
+
+export interface Module {
+  handlers: IonHandler | IonHandler[];
+  meta: ModuleMeta;
+  config?: object;
+}
+
+export interface LoadedModule {
+  meta: ModuleMeta;
+  config: object;
+}
+
 export default class extends Client {
   private prefixes: string[] = defaultPrefixes; //todo: load from database
-  public loadedModules: any[] = [];
+  public loadedModules: LoadedModule[] = [];
 
   constructor() {
     super();
   }
 
-  createPattern(text: string | RegExp) {
-    if (typeof text == "string") {
-      return new RegExp(
-        `^(${this.prefixes.filter(escapeForRegExp).join("|")})${escapeForRegExp(
-          text
-        )}`
-      );
+  createPattern(match: string | string[] | RegExp) {
+    if (match instanceof RegExp) {
+      return match;
     }
 
-    return text;
+    const commands = Array.isArray(match) ? match : [match];
+
+    return new RegExp(
+      `^(${this.prefixes.filter(escapeForRegExp).join("|")})(${commands
+        .filter(escapeForRegExp)
+        .join("|")})`
+    );
   }
 
   loadModules() {
@@ -36,37 +57,63 @@ export default class extends Client {
 
     this.loadedModules = []; // delete all previous loaded modules
 
-    allModules.map(async (mod) => {
-      const { meta } = mod;
-      const config: any = await moduleConfig.get(meta.slug);
-      let mode = {
-        outgoing: meta.mode === "outgoing",
-        incoming: meta.mode === "incoming",
-      };
+    modules.map(async (mod: Module) => {
+      const moduleMeta = mod.meta;
+      const config: any = await moduleConfig.get(moduleMeta.slug);
 
-      try {
-        this.client?.addEventHandler(
-          async (event: NewMessageEvent) => {
-            // Fetch config again from db for make changes live
-            const config: any = await moduleConfig.get(meta.slug);
-            mod.handler(event, config ? config.values : {});
-          },
-          new NewMessage({
-            ...mode,
-            func: (event) => {
-              return Boolean(
-                event.message.message?.match(this.createPattern(meta.match))
+      const handlers: IonHandler[] = Array.isArray(mod.handlers)
+        ? mod.handlers
+        : [mod.handlers];
+
+      handlers.forEach((handler) => {
+        const { meta } = handler;
+
+        meta.mode = meta.mode || "outgoing";
+        meta.scope = meta.scope || "all";
+
+        const mode = {
+          outgoing: meta.mode == "outgoing",
+          incoming: meta.mode == "incoming",
+        };
+
+        try {
+          this.client?.addEventHandler(
+            async (event: NewMessageEvent) => {
+              const config: any = await moduleConfig.get(moduleMeta.slug);
+              handler.handler(
+                this.client as TelegramClient,
+                event,
+                config ? config.values : {}
               );
             },
-          })
-        );
-      } catch (e) {
-        this.errorCount++;
-      }
+            new NewMessage({
+              ...mode,
+              func: (event) => {
+                const match = Boolean(
+                  event.message.message?.match(this.createPattern(meta.match))
+                );
+
+                switch (meta.scope) {
+                  case "private":
+                    return match && !!event.isPrivate;
+                  case "group":
+                    return match && !!event.isGroup;
+                  case "channel":
+                    return match && !!event.isChannel;
+                }
+
+                return match;
+              },
+            })
+          );
+        } catch (e) {
+          this.errorCount++;
+        }
+      });
 
       this.loadedModules.push({
-        ...meta,
-        configValues: config ? config.values : {},
+        meta: moduleMeta,
+        config: config ? config.values : {},
       });
     });
   }
--- packages/bot/src/ion/modules/eval/index.ts
+++ packages/bot/src/ion/modules/eval/index.ts
@@ -1,45 +1,7 @@
-import { NodeVM, VMScript } from "vm2";
-
-import { NewMessageEvent } from "telegram/events";
-
+import eval from "./eval";
 import meta from "./meta";
 
-const EvalModule = async (event: NewMessageEvent, config?: any) => {
-  const vm = new NodeVM({
-    console: "inherit",
-    sandbox: { event },
-    require: {
-      external: true,
-      builtin: ["fs", "path", "process", "os"],
-    },
-  });
-
-  let match = event.message.text.match(/^.eval ([\s\S]+)/m);
-  let code = "";
-
-  if (match) {
-    code = match[1];
-  }
-
-  // const before = Date.now();
-
-  let output = "";
-
-  try {
-    const script = new VMScript(`module.exports = ${code}`);
-    output = vm.run(script);
-    // const diff = Date.now() - before;
-  } catch (e) {
-    output = e;
-  }
-
-  event.message.edit({
-    text: `<code>${output}</code>`,
-    parseMode: "html",
-  });
-};
-
 export default {
-  handler: EvalModule,
+  handlers: eval,
   meta,
 };
